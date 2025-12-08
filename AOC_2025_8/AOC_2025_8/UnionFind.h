9f96dbb3fc7d49a59bf95c8386211796ac7efb23
Vasja Pavlov <vasjaguster@yahoo.com>
2025-12-08 13:52:25 +0100

    refactor

--- AOC_2025_8/AOC_2025_8/UnionFind.h
+++ AOC_2025_8/AOC_2025_8/UnionFind.h
@@ -1,11 +1,23 @@
 #ifndef UnionFind_h
 #define UnionFind_h
-
+#import <vector>
+using namespace std;
 // https://github.com/vasjapavlov/MyAlgorithms/blob/master/UnionFind.cpp
+struct Island {
+    int root, size;
+    Island(int _root, int _size) {
+        root = _root;
+        size = _size;
+    }
+    bool operator <(const Island &island) const {
+        return root > island.root;
+    }
+};
+
 class UnionFind {
 private:
-    int id[10000];
-    int sz[10000];
+    vector<int> id;
+    vector<int> sz;
     int getRoot(int node) {
         while(id[node] != node) {
             id[node] = id[ id[node] ];
@@ -15,7 +27,24 @@ private:
     }
     
 public:
+    vector<int> getIslandSizes() {
+        set<Island> islands;
+        for(auto x : id) {
+            int root = getRoot(x);
+            islands.insert(Island(root, sz[root]));
+        }
+        
+        vector<int> sizes;
+        for (const auto& island : islands) {
+            sizes.push_back(island.size);
+        }
+        sort(sizes.rbegin(), sizes.rend());
+        return sizes;
+    }
+
     UnionFind(int N) {
+        id = vector<int>(N);
+        sz = vector<int>(N);
         for(int i=0;i<N;i++) {
             id[i]=i;
             sz[i]=1;
@@ -29,7 +58,9 @@ public:
     int unite(int nodeA,int nodeB) {
         int root1=getRoot(nodeA);
         int root2=getRoot(nodeB);
-        
+        if(root1 == root2) {
+            return sz[root1];
+        }
         if(sz[root1] < sz[root2]) {
             id[root1] = root2;
             sz[root2] += sz[root1];
--- AOC_2025_8/AOC_2025_8/main.cpp
+++ AOC_2025_8/AOC_2025_8/main.cpp
@@ -14,126 +14,78 @@
 
 using namespace std;
 
-struct Point {
+struct Vertex {
     int i;
-    long long x,y,z;
+    int x,y,z;
 };
 
-struct Dist {
-    Point p1, p2;
+struct Edge {
+    Vertex p1, p2;
     long long distSq;
-    bool operator <(const Dist &d) const {
-        return distSq < d.distSq;
-    }
+
+    struct Comparator {
+        bool operator ()(const Edge &e1, const Edge &e2) {
+            return e1.distSq > e2.distSq;
+        }
+    };
 };
 
-long long distSq(Point p1, Point p2) {
-    long long x2 = p2.x - p1.x;
-    long long y2 = p2.y - p1.y;
-    long long z2 = p2.z - p1.z;
-    return x2 * x2 + y2 * y2 + z2 * z2;
+long long distSq(Vertex p1, Vertex p2) {
+    int dx = p2.x - p1.x;
+    int dy = p2.y - p1.y;
+    int dz = p2.z - p1.z;
+    return (long long)dx * dx + (long long)dy * dy + (long long)dz * dz;
 }
 
-bool vis[1001][1001];
-vector<int> graph[1001];
-int N;
-bool seen[1001];
-
-int dfs(int u) {
-    seen[u] = true;
-    int res = 1;
-    for(int i = 0; i < graph[u].size(); i++) {
-        int v = graph[u][i];
-        if(seen[v]) continue;
-        res += dfs(v);
-    }
-    return res;
-}
-
-long long solve(vector<Point> p) {
-    vector<Dist> d;
-    int n = (int)p.size();
-    
-    for(int i = 0; i < n; i++) {
-        for(int j = i+1; j< n; j++) {
-            Dist D;
-            D.p1 = p[i];
-            D.p2 = p[j];
-            D.distSq = distSq(p[i],p[j]);
-            d.push_back(D);
-        }
-    }
-    sort(d.rbegin(), d.rend());
-    
-    int cnt = 1000;
-    int pos = (int)d.size()-1;
-    while(cnt > 0 && pos >= 0) {
-        if(d.size() == 0) break;
-        Dist tmp = d[pos];
-        int u = tmp.p1.i;
-        int v = tmp.p2.i;
-        if(!vis[u][v]) {
-            graph[u].push_back(v);
-            graph[v].push_back(u);
-            vis[u][v] = vis[v][u] = true;
-        }
-        cnt--;
-        pos--;
-        
-    }
-    
-    vector<long long> circuitSizes;
-    memset(seen, false, sizeof seen);
-    for(int i = 0; i < N; i++) {
-        if(!seen[i]) {
-            long long circuitSize = dfs(i);
-            circuitSizes.push_back(circuitSize);
-        }
-    }
-    
-    long long res = 1;
-    sort(circuitSizes.rbegin(), circuitSizes.rend());
-    for(int i = 0; i < 3; i++) {
-        res *= circuitSizes[i];
-    }
-    return res;
-}
-
-long long solve2(vector<Point> p) {
-    
-    vector<Dist> d;
-    
-    int n = (int)p.size();
-    
-    for(int i = 0; i < n; i++) {
-        for(int j = i+1; j< n; j++) {
-            Dist D;
-            D.p1 = p[i];
-            D.p2 = p[j];
-            D.distSq = distSq(p[i],p[j]);
-            d.push_back(D);
-        }
-    }
-
-    sort(d.rbegin(), d.rend());
-    int pos = (int)d.size() - 1;
-    
-    auto unionFind = UnionFind(N);
-    while(pos >= 0) {
-        if(d.size() == 0) break;
-        Dist tmp = d[pos];
-        int u = tmp.p1.i;
-        int v = tmp.p2.i;
-        if(!unionFind.find(u, v)) {
-            int size = unionFind.unite(u, v);
-            cout << size << endl;
-            if(size == N) {
-                return (long long)tmp.p1.x * (long long)tmp.p2.x;
+priority_queue<Edge, vector<Edge>, Edge::Comparator>sortedEdges(vector<Vertex> points) {
+    auto n = points.size();
+    priority_queue<Edge, vector<Edge>, Edge::Comparator> q;
+        for(int i = 0; i < n; i++) {
+            for(int j = i+1; j< n; j++) {
+                Edge e;
+                e.p1 = points[i];
+                e.p2 = points[j];
+                e.distSq = distSq(points[i],points[j]);
+                q.push(e);
             }
         }
-        pos--;
-    }
+    return q;
+}
 
+long long solve(vector<Vertex> points) {
+    int n = (int)points.size();
+    auto q = sortedEdges(points);
+    auto unionFind = UnionFind(n);
+    
+    for(int steps = 0; steps < 1000; steps++) {
+        Edge e = q.top();
+        q.pop();
+        unionFind.unite(e.p1.i, e.p2.i);
+    }
+    long long res = 1;
+    vector<int> islandSizes = unionFind.getIslandSizes();
+    for(int i = 0; i < 3; i++) {
+        res *= islandSizes[i];
+    }
+    return res;
+}
+
+long long solve2(vector<Vertex> points) {
+    int n = (int)points.size();
+    auto q = sortedEdges(points);
+    auto unionFind = UnionFind(n);
+
+    while(!q.empty()) {
+        Edge tmp = q.top();
+        q.pop();
+        int u = tmp.p1.i;
+        int v = tmp.p2.i;
+        if(unionFind.find(u, v)) continue;
+        int circuitSize = unionFind.unite(u, v);
+        if(circuitSize == n) {
+            return (long long)tmp.p1.x * (long long)tmp.p2.x;
+        }
+    }
     return -1;
 }
 
@@ -142,28 +94,27 @@ int main(int argc, const char * argv[]) {
     ifstream fin("input");
     
     string s;
-    vector<Point> p;
+    vector<Vertex> points;
 
     int cnt = 0;
     while(getline(fin, s)) {
         vector<string> v = split(s, ",");
-        Point P;
+        Vertex p;
         
-        P.x = stoi(v[0]);
-        P.y = stoi(v[1]);
-        P.z = stoi(v[2]);
-        P.i = cnt++;
-        p.push_back(P);
+        p.x = stoi(v[0]);
+        p.y = stoi(v[1]);
+        p.z = stoi(v[2]);
+        p.i = cnt++;
+        points.push_back(p);
     }
-    N = cnt;
     
     
-    auto res1 = measureTime([&p]() {
-        return solve(p);
+    auto res1 = measureTime([&points]() {
+        return solve(points);
     });
     
-    auto res2 = measureTime([&p]() {
-        return solve2(p);
+    auto res2 = measureTime([&points]() {
+        return solve2(points);
     });
     
     cout << "Part 1: " << res1 << endl;
