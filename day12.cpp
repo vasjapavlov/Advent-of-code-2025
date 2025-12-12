bc462972a82f42acd1b7dbcd5cfeef5e9dedacb3
Vasja Pavlov <vasjaguster@yahoo.com>
2025-12-12 06:55:46 +0100

    add commenti

--- day12.cpp
+++ day12.cpp
@@ -9,6 +9,12 @@ int main(int argc, const char * argv[]) {
     
     string s;
     int res = 0;
+    /* Input assumes that shapes are not there since they are not needed for this solution.
+     Expects only regions like so:
+     4x4: 0 0 0 0 2 0
+     12x5: 1 0 1 0 2 2
+     12x5: 1 0 1 0 3 2
+    */
     while(getline(fin, s)) {
         vector<string> vs = split(s, ": ");
         int sum = 0;
